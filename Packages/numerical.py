28b9e6616ce42fcb7518199064795771b1b6361f
manju <n140007@rguktn.ac.in>
2019-06-20 10:26:35 +0530

    new files added

--- Packages/numerical.py
+++ Packages/numerical.py
@@ -1,25 +1,29 @@
-# Series of N prime numbers
+# Function to check if a given year is a leap year
+def isLeapYear(year):
+    if year%400==0 or(year%100!=0 and year%4==0):
+        return True
+    else:
+        return False
+isLeapYear(2020)
 
-def isPrime(N):
-    for i in range(2, N):
-        count =0
-        for j in range(1, i+1):
-            if(i%j==0):
-                count=count+1
-        if(count==2):
-            print(i, end = " ")
+
+
+
+# Function to print n Natural numbers using while loop
+def nNaturalNumbers(n):
+    counter = 1
+    while counter <= n:
+        print(counter, end=" ")
+        counter = counter + 1
     return
-N = int(input())
-isPrime(N)
+nNaturalNumbers(10)
 
 
 
-
-#squares of natural numbers
-n = int(input())
-def sumSquaresNaturalNumbers(n):
-    sum=0
-    for i in range(1, n+1):
-        sum=sum+i**2
-    return sum
-sumSquaresNaturalNumbers(n)
+# Function to check factors of a given number
+def factorial(n):
+    fact = 1
+    for i in range(2, n+1):
+        fact = fact * i
+    return fact
+factorial(4)
